780d6773c61f41ba238ea3a1d2d57f6a6cd268fa
vekariaayush04 <ayushrvekaria@gmail.com>
2025-01-10 23:50:53 +0530

    added ip

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -1,9 +1,14 @@
 import LeaderBoard from '@/components/LeaderBoard'
 import React from 'react'
 
-const page = () => {  
+const page =async () => {  
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/get-ip`, {
+    next: { revalidate: 0 }, // Ensure fresh fetch during development
+  });
+  const data: { ip: string } = await res.json();
   return (
     <div>
+        <h1>{data.ip}</h1>
         <LeaderBoard/>
     </div>
   )
