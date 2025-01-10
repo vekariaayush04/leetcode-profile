952d61b5306060cc3e0d615d2f04077686cfdd62
vekariaayush04 <ayushrvekaria@gmail.com>
2025-01-10 23:53:47 +0530

    added ip

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -2,7 +2,7 @@ import LeaderBoard from '@/components/LeaderBoard'
 import React from 'react'
 
 const page =async () => {  
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/get-ip`, {
+  const res = await fetch(`https://leetcode-profile-seven.vercel.app/api/get-ip`, {
     next: { revalidate: 0 }, // Ensure fresh fetch during development
   });
   const data: { ip: string } = await res.json();
